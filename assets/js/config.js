533ae4f08736e4a0c59856944499391153d6b0a9
BrianZoulko <bzoulko@gmail.com>
2022-09-12 21:38:44 -0700

    Everything works except for UV Index

---
+++ assets/js/config.js
@@ -0,0 +1,4 @@
+const cAPI_KEY = "0e232b663aad0248b5890600d1dacf38";
+const cGEOCODE_URL = "http://api.openweathermap.org/geo/1.0/direct?appid=" + cAPI_KEY;
+const cWEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=" + cAPI_KEY;
+const cFORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast?units=imperial&appid=" + cAPI_KEY;
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,23 @@
-var cityTemp = $("city-temp");
-var cityWind = $("city-wind");
-var cityHumidity = $("city-humidity");
-var cityUVIndex = $("city-uvindex");
-
-var day1Date = $("day1-date");
-var day2Date = $("day2-date");
-var day3Date = $("day3-date");
-var day4Date = $("day4-date");
-var day5Date = $("day5-date");
-
-var day1Icon = $("day1-weather-icon");
-var day2Icon = $("day2-weather-icon");
-var day3Icon = $("day3-weather-icon");
-var day4Icon = $("day4-weather-icon");
-var day5Icon = $("day5-weather-icon");
-
-var day1Temp = $("day1-city-temp");
-var day2Temp = $("day2-city-temp");
-var day3Temp = $("day3-city-temp");
-var day4Temp = $("day4-city-temp");
-var day5Temp = $("day5-city-temp");
-
-var day1Wind = $("day1-city-wind");
-var day2Wind = $("day2-city-wind");
-var day3Wind = $("day3-city-wind");
-var day4Wind = $("day4-city-wind");
-var day5Wind = $("day5-city-wind");
-
-var day1Humidity = $("day1-city-humidity");
-var day2Humidity = $("day2-city-humidity");
-var day3Humidity = $("day3-city-humidity");
-var day4Humidity = $("day4-city-humidity");
-var day5Humidity = $("day5-city-humidity");
+var cityName = $(".city-name");
+var cityTemp = $(".city-temp");
+var cityWind = $(".city-wind");
+var cityHumidity = $(".city-humidity");
+var cityUVIndex = $(".city-uvindex");
 
 var searchButtons = $("#search-buttons");
 var searchforCity = $("#searchfor-city");
 var primaryBtn = $("#primary-button");
 var secondaryBtn = $("secondary-buttons");
 
-
-// Search for button on click event for adding new cities.
-primaryBtn.on("click", addCityButton);
-
-// Refresh buttons saved in local storage.
-refreshButtons(searchButtons);
+var imgSun = "";
+var imgSun
 
 
 /* *********************
     Add new city button.
 ************************ */
-function addCityButton() {    
-    addButton(searchButtons, searchforCity.val());    
+function addCityButton() {
+    addButton(searchButtons, searchforCity.val(), true);    
     searchforCity.val("");
 }
 
@@ -68,40 +35,173 @@ function refreshButtons(container){
         keyValue = data.split(",");
         for (var x = 0; x < keyValue.length; x++) {
             if (keyValue[x] !== null && keyValue[x] !== '') {
-                addButton(container, keyValue[x]);
+                addButton(container, keyValue[x], false);
             }
         }
     }
+
+    // Update the dashboard with the first city in the list.
+    if (keyValue.length > 1) loadDashBoard(keyValue[1]);
+
 }
 
 
 /* ***********************************************************
     Update button container and local storage with new button.
 ************************************************************** */
-function addButton(container, caption) {    
+function addButton(container, city, bAdd) {    
     
+    // Search critera must have a value.
+    if (city === '') return;    
+
     // Declare new button variables.
     const key = "button";
     var keyValue = [""];
-    var buttonToAdd = $(`<${key}/>`, { type: key, class: "btn-block btn-secondary my-2", text: caption});
+    var buttonToAdd = $(`<${key}/>`, { type: key, class: "btn-block btn-secondary my-2", text: city});
 
     // Create/Append new button into container.
     container.append(buttonToAdd);
 
     // Create on click event for each button added.
     buttonToAdd.on("click", addCity);
-    function addCity(){
-        console.log("city clicked: " + buttonToAdd.text());
+    function addCity(){        
+        // Add City and Date to main weather dashboard.
+        loadDashBoard(buttonToAdd);
     }
 
     // Pull button list from local storage.
     var data = getData(key);
-    if (data !== null && data !== '') keyValue = data.split(",");
 
-    // Store new button list in local storage.
-    keyValue.push(caption);
-    putData(key, keyValue);
+    // Add City to main weather dashboard and local storage.
+    if (bAdd) {
 
+        if (data !== null && data !== '') keyValue = data.split(",");                
+        keyValue.push(city);
+        putData(key, keyValue);        
+        loadDashBoard(buttonToAdd);
+        
+    }
+
+}
+
+/*
+    Update dashboard with weather for a specific city and the next
+    5 days.
+*/
+function loadDashBoard(btn) {
+    const cMaxDays = 5;
+    var rtnCode = 0;
+
+    // Declare geocoding URL search variables.
+    var city = ((typeof btn == "string") ? btn : btn.text());
+    var geocodeURL = cGEOCODE_URL + "&q=" + city.replace(" ","%20") + ",,&limit=3";
+
+    fetch(geocodeURL).then(function(response){
+
+        return response.json();
+
+    }).then(function(data) {
+
+        rtnCode = parseInt(data.cod);
+        if (rtnCode < 200 || rtnCode > 299 || data.length == 0) {
+            alert("Error with (Geocode) city URL: /n/n" + geocodeURL);
+            removeCity(btn.text());
+            btn.remove();
+        } else {
+
+            console.log("rtnCode = " + rtnCode);
+            console.log("url: " + geocodeURL);
+
+            // Declare weather URL search variables.
+            var lattitude = data[0].lat;
+            var longitude = data[0].lon;
+            var weatherURL = cWEATHER_URL + "&q=" + city.replace(" ","%20") + "&lat=" + lattitude + "&lon=" + longitude;
+
+            fetch(weatherURL).then(function(response){
+
+                return response.json();
+
+            }).then(function(data) {
+                
+                rtnCode = parseInt(data.cod);
+                if (rtnCode < 200 || rtnCode > 299 || data.length == 0) {
+                    alert("Error with (Weather) URL: /n/n" + weatherURL);
+                    removeCity(btn.text());
+                    btn.remove();
+                } else {
+
+                    // Current City Weather.                    
+                    var cityIcon = $(`<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="Current weather image.">`);
+                    cityName.text(city + " (" + getDate() + ")");
+                    cityName.append(cityIcon);
+                    cityTemp.text("Temp: " + data.main.temp + " \xB0F");
+                    cityWind.text("Wind: " + data.wind.speed + " MPH");
+                    cityHumidity.text("Humidity: " + data.main.humidity + " %");
+                    cityUVIndex.text("UV Index: ");                    
+
+                    // // Declare 5-Day Forecast URL search variables.
+                    var forecastURL = cFORECAST_URL + "&lat=" + lattitude + "&lon=" + longitude + "&cnt=" + cMaxDays ;
+
+                    fetch(forecastURL).then(function(response){
+
+                        return response.json();
+
+                    }).then(function(data) {
+                        
+                        rtnCode = parseInt(data.cod);
+                        if (rtnCode < 200 || rtnCode > 299 || data.length == 0) {
+                            alert("Error with (Forecast) URL: /n/n" + forecastURL);
+                            removeCity(btn.text());
+                            btn.remove();
+                        } else {
+                            
+                            console.log("url: " + forecastURL);
+                            // Set (x)Days Forecast    
+                            for (var d = 0; d < cMaxDays; d++) {
+                                var i = d+1;
+                                var forecastIcon = $(`<img src="https://openweathermap.org/img/wn/${data.list[d].weather[0].icon}.png" alt="Forecast weather icon.">`);
+                                $(`.day${i}-date`).text(getDate(i));                                
+                                $(`.day${i}-date`).append(forecastIcon);
+                                $(`.day${i}-weather-icon`).attr("src", );
+                                $(`.day${i}-city-temp`).text("Temp: " + data.list[d].main.temp + " \xB0F");
+                                $(`.day${i}-city-wind`).text("Wind: " + data.list[d].wind.speed + " MPH");
+                                $(`.day${i}-city-humidity`).text("Humidity: " + data.list[d].main.humidity + " %");
+                            }
+
+                        }
+
+                    })
+
+                }
+
+            })
+
+            
+
+        }
+    })
+    
+}
+
+
+/* ********************************
+    Remove city from local storage.
+*********************************** */
+function removeCity(city) {
+    // Remove city from local storage.
+    const key = "button";
+    var data = getData(key).replace("," + city,"");
+    putData(key, data); 
+}
+
+
+/* **************************************************
+    Obtain formatted date for weather dashboard city.
+***************************************************** */
+function getDate(incByDay){
+    var numberOfDays = parseInt(incByDay);
+    var useDate = moment().add(numberOfDays, 'days');
+    return(useDate.format('MM/DD/YYYY'));
 }
 
 
@@ -109,7 +209,7 @@ function addButton(container, caption) {
     Store data in localStorage based on a key and the value or data being passed.
 ******************************************************************************* */
 function putData(key, value){
-localStorage.setItem(key, value);
+    localStorage.setItem(key, value);
 }
 
 
@@ -117,7 +217,12 @@ localStorage.setItem(key, value);
     Return the value for a specific key in local storage.
 ******************************************************* */
 function getData(key) {
-return(localStorage.getItem(key));
+    return(localStorage.getItem(key));
 }
 
 
+// Search for button on click event for adding new cities.
+primaryBtn.on("click", addCityButton);
+
+// Refresh buttons saved in local storage.
+refreshButtons(searchButtons);
