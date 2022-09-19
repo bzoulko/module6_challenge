5503a777d21bed96883491e720174b1495600a6d
BrianZoulko <bzoulko@gmail.com>
2022-09-19 07:28:20 -0700

    Split api-key into an array so the application in functional in deployment.

--- assets/js/config.js
+++ assets/js/config.js
@@ -13,7 +13,7 @@
                                API keys.    
 */
 
-const cAPI_KEY = "0e232b663aad0248b5890600d1dacf38";
-const cGEOCODE_URL = "http://api.openweathermap.org/geo/1.0/direct?appid=" + cAPI_KEY;
-const cWEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=" + cAPI_KEY;
-const cFORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast?units=imperial&appid=" + cAPI_KEY;
+const cAPI_KEY = ["0e232","b663a","ad024","8b589","0600d","1dacf","38"];
+const cGEOCODE_URL = "http://api.openweathermap.org/geo/1.0/direct?appid=" + cAPI_KEY.join("");
+const cWEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=" + cAPI_KEY.join("");
+const cFORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast?units=imperial&appid=" + cAPI_KEY.join("");
